8f4a8377f7a2050653b1106aa9a885fcb09a0822
mariselvam07 <rlmselvam7@gmail.com>
2022-11-15 13:09:09 +1400

    added console

---
+++ 02_script.js
@@ -0,0 +1,19 @@
+// F12 to view console
+console.log("Welcome to JS");   // string
+console.log(123456);            // number
+console.log(11.11);             // decimal
+console.log(true);              // boolean
+console.log([1,35,25,75]);      // array
+console.log({fname:'Mari', age:25});                     // json value or object and value or key pairs
+console.table({fname:'Mari', age:25});                     // in table formate json value or object and value or key pairs
+console.error("Custom Sample Error");
+console.warn("Warning to Custom");
+// console.clear();       // clear date
+console.time("Timer");
+for(i=0;i<10;i++)
+{
+    console.log(i)
+}
+console.timeEnd("Timer");
+
+
