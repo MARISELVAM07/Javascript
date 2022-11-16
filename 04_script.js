14998caa022057141cad1e2e7a41fa8784d78358
mariselvam07 <rlmselvam7@gmail.com>
2022-11-16 20:54:48 +1400

    added data types

---
+++ 04_script.js
@@ -0,0 +1,32 @@
+// Data Types in Javascript
+/*
+JS Dynamic Program
+
+Premitive Data Type
+    1. Number       eg: 25, 1.25
+    2. String
+    2. Boolean      eg: True, False
+    3. Null
+    4. Undefined    
+    5. Symbols  -   E6 (New) 2015
+
+Reference Data Type
+    1. Array
+    2. Object Literals
+    3. Date and Time
+    4. Function
+
+*/
+
+// Number Type
+
+var a=25;
+console.log(typeof a);
+
+a=25.5;
+console.log(typeof a);
+
+var Fname="Mariselvam";
+console.log(typeof Fname);
+
+
