91745b8eabf276632dc2ab68c4df508703c20389
mariselvam07 <rlmselvam7@gmail.com>
2022-11-18 19:13:27 +1400

    updated Type coercion

---
+++ 06_script.js
@@ -0,0 +1,15 @@
+// TYPE COERCION
+
+var a="25";
+var b=15;
+console.log(a+b); // concodination
+console.log(typeof a+b);
+
+a=parseInt(a); // Method - 1
+console.log(a+b);
+console.log(typeof a+b);
+
+a=Number(a); // Method - 2
+console.log(a+b);
+console.log(typeof a+b);
+
