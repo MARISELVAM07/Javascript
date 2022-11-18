06b8f96e0a8e26c30d46cec821a7ce865c8a70e8
mariselvam07 <rlmselvam7@gmail.com>
2022-11-19 11:17:20 +1400

    updated Arithemetic Operation

---
+++ 07_script.js
@@ -0,0 +1,40 @@
+// ARITHMETIC OPERATION
+
+let a=100;
+let b=20;
+let c;
+
+//Addition
+c=(a+b);
+console.log(c);
+console.log(typeof c);
+
+//Subtraction
+c=(a-b);
+console.log(c);
+
+//Multiplication
+c=(a*b);
+console.log(c);
+
+//Division
+c=(a/b);
+console.log(c);
+
+//modulus
+c=(a%b);
+console.log(c);
+
+//power  (2016)
+c=(15**2);
+console.log(c);
+
+//Increment & Decrement
+c=++a;
+console.log(c);
+c=--b;
+console.log(c);
+
+
+
+
