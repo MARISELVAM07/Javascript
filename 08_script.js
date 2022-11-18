d91a1b5ca4c39379350a3f57bd3e8ef8c0eef1cd
mariselvam07 <rlmselvam7@gmail.com>
2022-11-19 12:25:59 +1400

    updated Assignment Operators

---
+++ 08_script.js
@@ -0,0 +1,26 @@
+// ASSIGNMENT OPERATOS
+
+let a=20;
+// a=a+5;  // +=
+
+//adding
+a+=5; 
+console.log(a);
+
+//subtraction
+a-=5;
+console.log(a);
+
+//multiplication
+a*=5;
+console.log(a);
+
+//division
+a/=5;
+console.log(a);
+
+//modulus
+a%=5;
+console.log(a);
+
+
