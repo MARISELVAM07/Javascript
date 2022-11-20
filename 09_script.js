b93d7ae5829c2a4e791b71d0ffdc2e6fb2a98413
mariselvam07 <rlmselvam7@gmail.com>
2022-11-20 22:53:56 +1400

    updated math functions

--- 09_script.js
+++ 09_script.js
@@ -1,2 +1,41 @@
 // MATH FUNCTIONS
 
+let c;
+// pi value
+c=Math.PI;
+console.log(c);
+console.log(typeof c);
+
+// e - Euler's value
+c=Math.E;
+console.log(c);
+
+// Round off
+c=Math.round(75.8);
+console.log(c);
+c=Math.round(75.3);
+console.log(c);
+
+// Floor (remove decimal)
+c=Math.floor(8.7325);
+console.log(c);
+
+//trunc (remove decimal)
+c=Math.trunc(2.85);
+console.log(c);
+
+//ceil (5.1 = 6, 5.00001 = 6)
+c=Math.ceil(55.000001);
+console.log(c);
+
+//square root
+c=Math.sqrt(110);
+console.log(c);
+
+//abs( negative to positive)
+c=Math.abs(-77);
+console.log(c);
+
+//Power value
+c=Math.pow(2,4);
+console.log(c);
