fbf084211bc2ccc4b2a6bb3d7e1487f61a4b6ed1
mariselvam07 <rlmselvam7@gmail.com>
2022-11-21 22:12:25 +1400

    updated Relational Operartors

---
+++ 11_script.js
@@ -0,0 +1,22 @@
+// RELATIONAL OPERATORS 
+
+/*
+    1. > Greater than
+    2. < Lesser than
+    3. >= Greater than or equal to
+    4. <= Less then or equal to
+*/
+// Greater & Lesser
+let a=10;
+let b=20;
+console.log("Greater:", a>b);
+console.log("Lesser:",a<b);
+
+/*----------------------------*/
+
+//Greater Than Equal & Lesser Than Equal
+a=10;
+b=20;
+console.log("Greater Than Equal:", a>=b);
+console.log("Lesser Than Equal:", a<=b );
+
