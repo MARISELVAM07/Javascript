a6065c14635cbec509f5ad232e4d43e4b8eeafec
mariselvam07 <rlmselvam7@gmail.com>
2022-11-21 15:06:29 +1400

    updated Comparison Operator

--- 10_script.js
+++ 10_script.js
@@ -1,1 +1,31 @@
-alert("welcome");
+// COMPARISON OPERATORS
+ 
+
+// Double == operator (checks only value)
+let a=10;
+let b=20;
+console.log(a==b); // diff. value so false
+
+a=10;
+b=10;
+console.log(a==b); // same value so true
+
+a=10;
+b="10";
+console.log(a==b); // Diff type but same value so true
+
+
+
+// Triple === Operator (checks value and type)
+
+a="10";
+b="10";
+console.log(a===b); // same type same value so true
+
+a="10";
+b="20";
+console.log(a===b);// same type but diff. value so false
+
+a=10;
+b="10";
+console.log(a===b); // diff. type but same value so false
