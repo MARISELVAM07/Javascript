159368acf2b37c6d2c2f540f1f8e0ed0a82ceb83
mariselvam07 <rlmselvam7@gmail.com>
2022-11-22 22:30:37 +1400

    updated Logical operators

--- 12_script.js
+++ 12_script.js
@@ -8,6 +8,16 @@
 
 // Pass Mark - (35-100)
 let mark=45;
+console.log(mark>=35 && mark<=100);
 
+mark=20;
+console.log(mark>=35 && mark<=100);
+
+
+// The Number must be 2 or 5
+let Number=2;
+console.log(Number==2 || Number==5);
+Number=3;
+console.log(Number==2 || Number==5);
 
 
